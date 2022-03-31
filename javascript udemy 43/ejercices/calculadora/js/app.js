0a6e2e7defb8cd0b00c75860d13fe5885f638cc8
iwinser aljadys sanchez <77251836+iwinser117@users.noreply.github.com>
2022-03-31 17:55:58 -0500

    47

--- javascript udemy 43/ejercices/calculadora/js/app.js
+++ javascript udemy 43/ejercices/calculadora/js/app.js
@@ -1,10 +1,10 @@
 console.log(`aplicacion calculadoraa`)
 function sumar(){
-    const forma = document.getElementById(`forma`);
+    const forma = document.getElementById("forma");
     let operandoA = forma[`operandoA`];
-    let operandoB = forma[`operandoA`];
+    let operandoB = forma[`operandoB`];
     let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
-    if (isNaN) (resultado)
+    if (isNaN(resultado))
         resultado = `la operacion le faltan numeros`;
-    document.getElementById(`resultado`).innerHTML = `resultado ${resultado}`;    
+    document.getElementById(`resultado`).innerHTML = `resultado ${resultado}`;
 }
