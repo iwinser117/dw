3513fd35688a17aad1cce6a89075a55010b788dd
iwinser aljadys sanchez <77251836+iwinser117@users.noreply.github.com>
2022-04-07 20:16:03 -0500

    47

--- javascript udemy 43/ejercices/calculadora/js/app.js
+++ javascript udemy 43/ejercices/calculadora/js/app.js
@@ -1,10 +1,11 @@
-console.log(`aplicacion calculadoraa`)
+console.log("hola");
 function sumar(){
-    const forma = document.getElementById("forma");
+    const forma = document.getElementById(`forma`);
     let operandoA = forma[`operandoA`];
     let operandoB = forma[`operandoB`];
     let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
     if (isNaN(resultado))
         resultado = `la operacion le faltan numeros`;
-    document.getElementById(`resultado`).innerHTML = `resultado ${resultado}`;
+    document.getElementById(`resultado`).innerHTML = `resultado : ${resultado}`;
+    console.log(`resultado : ${resultado}`);
 }
--- javascript udemy 43/ejercices/calculadora/js/dist/app.dev.js
+++ javascript udemy 43/ejercices/calculadora/js/dist/app.dev.js
@@ -1,13 +1,13 @@
 "use strict";
 
-console.log("aplicacion calculadoraa");
+console.log("hola");
 
 function sumar() {
   var forma = document.getElementById("forma");
   var operandoA = forma["operandoA"];
-  var operandoB = forma["operandoA"];
+  var operandoB = forma["operandoB"];
   var resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
-  if (isNaN) resultado;
-  resultado = "la operacion le faltan numeros";
-  document.getElementById("resultado").innerHTML = "resultado ".concat(resultado);
+  if (isNaN(resultado)) resultado = "la operacion le faltan numeros";
+  document.getElementById("resultado").innerHTML = "resultado : ".concat(resultado);
+  console.log("resultado : ".concat(resultado));
 }
