6be190c3c62db84d66e2361b78de38960b2a33cd
iwinser sanchez <iwinser.sanchez@gmail.com>
2022-05-01 22:55:09 -0500

    un cambio expontanta

--- python_CC/sesion_1/1_salida.py
+++ python_CC/sesion_1/1_salida.py
@@ -1,5 +1,4 @@
 # estructura de salida
 # print ("texto")  o tambien print ("texto")
 
-print ("phdhhsdhdsh \n hola esto es   texto")
-print("un ejemplo de suma" ,8 + 5)
+print("phdhhsdhdsh \n hola esto es texto")
