d2d0a5eda71357886806697d24ad44b549fccece
iwinser sanchez <iwinser.sanchez@gmail.com>
2022-05-06 23:59:02 -0500

    volviendo a programar

--- Universidad de antioquia/desarrollando1.py
+++ Universidad de antioquia/desarrollando1.py
@@ -1,3 +1,7 @@
-opcion = ["1:Densidad ","2:presion de un fluido", "3:principio de arquimedes"]
-print(opcion)
-# ingrese_opcion= int(input("{opcion}ingrese una opcion"))
+animales_domesticos = set (["perro", "gato", "conejo"])
+print(animales_domesticos, "de tipo" , type(animales_domesticos))
+
+print(f"el conjunto tiene {len(animales_domesticos)} elementos")
+
+animales_domesticos.__sub__("gatito")
+print(animales_domesticos)
