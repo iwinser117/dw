c519fcc641cb197f890f368730c7ba8cd09a1872
iwinser sanchez <iwinser.sanchez@gmail.com>
2022-05-09 22:08:27 -0500

    parece que si

--- Universidad de antioquia/reto21.py
+++ Universidad de antioquia/reto21.py
@@ -5,49 +5,25 @@ porcentajesAcum = 0
 notasAcum = 0
 opcion = "s"
 while porcentajesAcum < 100 and opcion == "s":
-    notaObtenida = float(input("Ingrese la nota obtenida: "))
-    porcentajeNota = int(input("Ingrese el porcentaje de la nota: "))
-    notasAcum += notaObtenida
-    porcentajesAcum = porcentajesAcum + porcentajeNota
-    if opcion == "s":
-        opcion = input("¿Falta añadir mas notas? S/N ")
-    if opcion == "n":
-        print("maracste n")
-        break
-    elif porcentajesAcum > 100:
-        notasAcum = notasAcum - notaObtenida
-        porcentajesAcum = porcentajesAcum - porcentajeNota
-        print(porcentajesAcum)
-        print("el porcentaje evaluado de una materia no puede mayor a 100")
-
-    elif porcentajesAcum == 100:
-        print("hola mundo")
-        break
-
-
-print("es dfife")
-print(porcentajesAcum)
-print(notasAcum)
-
-
-else:
-    opcion = input("¿Falta añadir mas notas? S/N ")
-
-
-print('¡Bienvenido!En esta aplicacin los estudiantes podran gestionar las notas de su materia.')
-nombreEstudiante = input("Por favor ingrese su nombre:")
-nombreMateria = input("Ingrese el nombre de la materia: ")
-porcentajesAcum = 0
-notasAcum = 0
-opcion = "s"
-if porcentajesAcum < 100 and opcion == "s":
+    while porcentajesAcum < 100 and opcion == "s":
+        notaObtenida = float(input("Ingrese la nota obtenida: "))
+        porcentajeNota = int(input("Ingrese el porcentaje de la nota: "))
+        notasAcum += notaObtenida
+        porcentajesAcum = porcentajesAcum + porcentajeNota
+        if porcentajesAcum < 100:
+            opcion = input("¿Falta añadir mas notas? S/N ")
+            continue
     while porcentajesAcum > 100:
         notasAcum = notasAcum - notaObtenida
         porcentajesAcum = porcentajesAcum - porcentajeNota
         print(porcentajesAcum)
         print("el porcentaje evaluado de una materia no puede mayor a 100")
-        notaObtenida = float(input("Ingrese la nota obtenida: "))
-        porcentajeNota = int(input("Ingrese el porcentaje de la nota: "))
-        notasAcum += notaObtenida
-        porcentajesAcum = porcentajesAcum + porcentajeNota
-        opcion = input("¿Falta añadir mas notas? S/N ")
+if porcentajesAcum == 100:
+    print("hola mundo")
+elif opcion == "n":
+    print("maracste n")
+
+
+print("es dfife")
+print(porcentajesAcum)
+print(notasAcum)
