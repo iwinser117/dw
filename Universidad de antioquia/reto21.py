ccdec4a2f5f7c1995612463eb3a6f3c4e4deac13
iwinser sanchez <iwinser.sanchez@gmail.com>
2022-05-09 22:00:26 -0500

    parece que si

--- Universidad de antioquia/reto21.py
+++ Universidad de antioquia/reto21.py
@@ -28,3 +28,26 @@ while porcentajesAcum < 100 and opcion == "s":
 print("es dfife")
 print(porcentajesAcum)
 print(notasAcum)
+
+
+else:
+    opcion = input("¿Falta añadir mas notas? S/N ")
+
+
+print('¡Bienvenido!En esta aplicacin los estudiantes podran gestionar las notas de su materia.')
+nombreEstudiante = input("Por favor ingrese su nombre:")
+nombreMateria = input("Ingrese el nombre de la materia: ")
+porcentajesAcum = 0
+notasAcum = 0
+opcion = "s"
+if porcentajesAcum < 100 and opcion == "s":
+    while porcentajesAcum > 100:
+        notasAcum = notasAcum - notaObtenida
+        porcentajesAcum = porcentajesAcum - porcentajeNota
+        print(porcentajesAcum)
+        print("el porcentaje evaluado de una materia no puede mayor a 100")
+        notaObtenida = float(input("Ingrese la nota obtenida: "))
+        porcentajeNota = int(input("Ingrese el porcentaje de la nota: "))
+        notasAcum += notaObtenida
+        porcentajesAcum = porcentajesAcum + porcentajeNota
+        opcion = input("¿Falta añadir mas notas? S/N ")
