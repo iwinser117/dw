58e71fb686a8e5473bd7a0611180229ffc0df63d
iwinser <iwinser.sanchez47@gmail.com>
2022-05-19 19:41:25 -0500

    listo el reto 1

--- Universidad de antioquia/reto1.py
+++ Universidad de antioquia/reto1.py
@@ -1,27 +1,27 @@
-print("¡Bienvenido! En esta aplicación los estudiantes podran gestionar las notas de su materia.")
-nameEstudiante = input("Por favor ingrese su nombre: ")
-nameMateria = input("Ingrese el nombre de la materia: ")
-opcion = "s"
-notasAcum = 0
-porcentajesAcum = 0
-while opcion == "s":
-    notaObtenida = float(input("Ingrese la nota obtenida: "))
-    porcentajeNota = float(input("Ingrese el porcentaje de la nota: "))
-    notasAcum += notaObtenida
-    porcentajesAcum += porcentajeNota
-    notaPromedio = (notasAcum * porcentajesAcum) / 100
-    if porcentajesAcum > 100:
-            print("El porcentaje evaluado de una materia no puede ser mayor a 100")
-            notasAcum = notasAcum - notaObtenida
-            porcentajesAcum = porcentajesAcum - porcentajeNota
-            continue
-    elif porcentajesAcum == 100:
-            reportFinal = print(f"El estudiante {nameEstudiante.capitalize()} cursó la materia {nameMateria.capitalize()} ")
+print("¡Bienvenido! En esta aplicación los estudiantes podrán gestionar las notas de su materia.")
+name_student = input("Por favor ingrese su nombre: ")
+name_topic = input("Ingrese el nombre de la materia: ")
+option = "S"
+percent_collector = 0
+collector_average = 0
+while option == "S":
+    grade_earned = float(input("Ingrese la nota obtenida: "))
+    percent_grade = float(input("Ingrese el porcentaje de la nota: "))
+    percent_collector += percent_grade
+    if percent_collector > 100:
+        print("El porcentaje evaluado de una materia no puede ser mayor a 100")
+        percent_collector -= percent_grade
+        continue
+    if percent_collector <= 100:
+        collector_average += grade_earned * percent_grade
+        result_grade = "reprobado"
+        grade_average = collector_average / 100
+        if (grade_average >= 3): result_grade = "aprobado"
+        if percent_collector == 100:
+            print(
+                f"El estudiante {name_student.capitalize()} cursó la materia {name_topic.capitalize()} y obtuvo {grade_average:.1f} resultando en {result_grade}")
             break
-    elif porcentajesAcum < 100 and opcion == "s":
-            opcion = input("¿Falta añadir mas notas? S/N ")
-            if opcion == "n":
-                reportFinal = print(
-                    f"El estudiante {nameEstudiante.capitalize()} cursó la materia {nameMateria} y")
+        option = input("¿Falta añadir notas? S/N ")
+        if option == "N":
+                print(f"El estudiante {name_student.capitalize()} cursó la materia {name_topic.capitalize()} y obtuvo {grade_average:.2f} resultando en {result_grade}")    
                 break
-           
