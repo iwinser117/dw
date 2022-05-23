9e20cef4c56c7b5beaa004dde5f0578a337e9958
iwinser <iwinser.sanchez47@gmail.com>
2022-05-23 18:51:42 -0500

    kjkj

--- Universidad de antioquia/reto2.py
+++ Universidad de antioquia/reto2.py
@@ -5,43 +5,32 @@
 def modificar_materia(pensum, semestre, materia, nombre, creditos):
     # ACÁ INICIA LA FUNCIÓN modificar_materia (En este espacio debes
     # poner el código necesario para implementar esta funcionalidad)
-    creditos = 0
-    semestre = 0
-    mensaje = "Modificada con éxito"
-    nombre = " "
-    materia = 0
     pensum = [
         {'0123': {'nombre': 'intro a la ing', 'créditos': 2},
          '4567': {'nombre': 'inglés', 'créditos': 1}},
         {},
         {}
     ]
-    while semestre == 1:
-      semestre = int(input("Ingrese el semestre a Evaluar:"))
-      semestre -= 1
-      if semestre < 0 or semestre > 2:
-          print('Ingrese un semestre válido')
-      elif semestre > 0 and semestre <= 2:
-          print("el semestre no tiene materias")
-      if semestre == 0:
-          materia = str(input('Ingrese el codigo de la materia: '))
-      # elif materia != '0123':
-      #    print('La materia no existe')
-          if materia == "0123":
-              nom_materia = str(input('Ingrese el nombre de la materia que desea modificar '))
-              creditos = int(input('Ingrese el numero de creditos de la materia que modifico '))
-              #pensum[0]['0123']['nombre'] = nombre
-              pensum[0]["0123":{"nombre"}.append(nombre)]
-              pensum[0]['0123']['creditos'] = creditos
+    semestre = int(0)
+    materia = str(0)
+    nombre = 0
+    creditos = 0
+    mensaje = "Modificada con éxito"
 
-              pensum = [
-                  {'0123': {'creditos': creditos, 'nombre': nombre},
-                   '4567': {'creditos': 1, 'nombre': 'inglés'}},
-                  {},
-                  {}]
-              print(pensum)
-              break
-          print(mensaje)
+    semestre = int(input("Ingrese el Semestre a Evaluar: "))
+    semestre -= 1
+    if semestre < 0 or semestre > 2:
+        mensaje = "Ingrese un semestre válido"
+        print(mensaje)
+    elif semestre > 0 and semestre <= 2:
+        mensaje = "El semestre no tiene materias"
+        print(mensaje)
+    elif semestre == 0:
+        materia = input('Ingrese el codigo de la materia')
+        if materia != "0123":
+            mensaje = "La materia no existe"
+            print(mensaje)
+        print(mensaje)
 
     # ACÁ TERMINA LA FUNCIÓN modificar_materia
     # NO MODIFIQUES LA SIGUIENTE LÍNEA
