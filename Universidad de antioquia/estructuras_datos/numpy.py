d7f12b19c77427694279cf0a34d90a2aa58fa23b
iwinser <iwinser.sanchez47@gmail.com>
2022-05-28 20:47:11 -0500

    installando numpy

---
+++ Universidad de antioquia/estructuras_datos/numpy.py
@@ -0,0 +1,11 @@
+import numpy as np 
+l=[1,2, 3]
+m=[4, 5 ,6]
+
+#generando un arreglo
+arreglo=np.array([1, 2, 3])
+arreglo2=np.array([4, 5, 6])
+
+print(f"{l}")
+print(f"{arreglo2}")
+print(type(arreglo2))
