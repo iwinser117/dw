fb581b79a54334651a4e1456c52214cb7ed23275
iwinser <iwinser.sanchez47@gmail.com>
2022-06-04 12:05:11 -0500

    jj

---
+++ Soluciones retos js/ret4/reto4_encriptado_cesar.py
@@ -0,0 +1,34 @@
+abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+       'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+mensaje = "U DE A"
+b = 7
+def encriptar_caracter(caracter, b):
+    caracter_encriptado= ""
+    for letra in caracter:
+        suma = abc.index(letra)+b
+        modulo = int (suma) % len(abc)
+        caracter_encriptado=caracter_encriptado +str(abc[modulo])
+    return caracter_encriptado
+v=encriptar_caracter("A",7)
+print(v)
+
+
+l1=[1, 2, 3, 4]
+l2 = list(map(lambda x: x**2,l1))
+print(l2)
+
+
+"""def encriptar(mensaje, b):
+
+    return mensaje_encriptado
+
+
+def desencriptar_caracter(caracter_encriptado, b):
+
+    return caracter_desencriptado
+
+
+def desencriptar(mensaje_encriptado, b):
+
+    return mensaje_desencriptado
+"""
