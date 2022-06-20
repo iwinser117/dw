c14fe71c3a0e7dc1fbc25c830f19cecf1be5ff5a
iwinser <iwinser.sanchez47@gmail.com>
2022-06-20 16:09:12 -0500

    k

--- Universidad de antioquia/java/ciclo2/Ejemplo.java
+++ Universidad de antioquia/java/ciclo2/Ejemplo.java
@@ -18,5 +18,19 @@ public class Ejemplo {
         String employeeName = "Iwinser";
         employeeName = employeeName + " Sanchez";
         System.out.println("tu nombre es: " + employeeName);
+
+        //camel case
+        String fullName = "Iwinser Aljadys Sanchez";
+        int sizeInCentimeters = 50;
+        System.out.println(fullName + sizeInCentimeters);
+
+        //char
+        //utiliza comilllas simples al final
+
+        var salario = 1000;
+        var pension = salario*0.03;
+
+        System.out.println(salario);
+        Systema.out.println(pension);
     }
 }
