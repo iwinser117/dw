11285bf123f0b0f772e998b4a8768e268c423483
iwinser <iwinser.sanchez47@gmail.com>
2022-06-20 16:42:58 -0500

    sensible a mayusculas

--- Universidad de antioquia/java/ciclo2/Ejemplo.java
+++ Universidad de antioquia/java/ciclo2/Ejemplo.java
@@ -3,11 +3,20 @@
  */
 public class Ejemplo {
     public static void main(String[] args) {
-        System.out.println("hola mi nombre es iwinser");
-        int a = 1;
-        int b = 27;
-        int c = 8;
-        int m = 19;
-        System.out.println(a+b*m/c);
+        int salary = 1000;
+
+        salary = salary + 200;
+        System.out.println(salary);
+
+        salary = salary - 50;
+        System.out.println(salary);
+
+        salary = salary + (30 * 2) - 45;
+        System.out.println(salary);
+
+        // actualizando cadenas de texto
+        String employeeName = "Iwinser";
+        employeeName = employeeName + " Sanchez";
+        System.out.println("tu nombre es: " + employeeName);
     }
 }
