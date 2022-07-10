c00f7910dde93705b63aeb262a26cb4134149371
iwinser <iwinser.sanchez47@gmail.com>
2022-07-10 10:28:23 -0500

    ok

--- Soluciones retos js/dist/solucioReto1.dev.js
+++ Soluciones retos js/dist/solucioReto1.dev.js
@@ -1,1 +1,20 @@
-"use strict";
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'},
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo'}
+]
+/* 
+Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios. */
+
+let ovejasRojas;
+
+for (let i = 0; i < ovejas.length; i++) {
+    //console.log(ovejas[i].name);
+    if(ovejas[i].color == "rojo"){
+        i.filter(ovejas.name.includes("a", "A","n"));
+        console.log(ovejas[i]);
+    }
+}
