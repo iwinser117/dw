9d0af381d1c09db7f977351b3e1399c43487e748
iwinser <iwinser.sanchez47@gmail.com>
2022-08-05 17:54:41 -0500

    l

--- .vscode/Java/1java.java
+++
@@ -1,7 +0,0 @@
-public class sumar {
-    public static void name(String[] args) {
-        String textoUno = "este es el primer texto";
-        String textoDos = "este es el segundo texto";
-        
-    }
-}
