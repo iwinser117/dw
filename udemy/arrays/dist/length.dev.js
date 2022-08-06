74091828aa4e8da6b438ab5c2f6171de747dc310
iwinser <iwinser.sanchez47@gmail.com>
2022-08-06 18:20:53 -0500

    ejerciendo arrays

---
+++ udemy/arrays/dist/length.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var varios = [117, "iwinser", {
+  nombre: 1,
+  apellido: "sanchez"
+}, true, false, 2321, [1, 23, "tree"]];
---
+++ udemy/arrays/length.js
@@ -0,0 +1,9 @@
+const varios = [
+    117,
+    "iwinser",
+    {nombre : 1 , apellido :"sanchez"},
+    true,
+    false,
+    2321,
+    [1, 23, "tree"]
+];
