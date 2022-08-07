f73af772ef51cfe9bf3ed961a3878fbd52939f96
iwinser <iwinser.sanchez47@gmail.com>
2022-08-07 13:56:14 -0500

    k

--- udemy/arrays/dist/length.dev.js
+++ udemy/arrays/dist/length.dev.js
@@ -4,9 +4,9 @@ var varios = [117, "iwinser", {
   nombre: 1,
   apellido: "sanchez"
 }, true, false, 2321, [1, 23, "tree"]]; //poniendo varios.length accedemos a la cantidad de elementos que tiene nuesstro array
-
-console.log(varios.length); //pero este varios.length se puede redefinir ... por ejemplo mire a acontinuacion
+//console.log(varios[2]);
+//pero este varios.length se puede redefinir ... por ejemplo mire a acontinuacion
 
 varios.length = 11; //y al imprimir el array se deja espacio para 4 item
 
-console.log(varios);
+console.log(varios[6].length);
--- udemy/arrays/length.js
+++ udemy/arrays/length.js
@@ -8,9 +8,9 @@ const varios = [
     [1, 23, "tree"]
 ];
 //poniendo varios.length accedemos a la cantidad de elementos que tiene nuesstro array
-console.log(varios.length);
+//console.log(varios[2]);
 
 //pero este varios.length se puede redefinir ... por ejemplo mire a acontinuacion
 varios.length = 11;
 //y al imprimir el array se deja espacio para 4 item
-console.log(varios);
+console.log(varios[6].length);
