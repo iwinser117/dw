75944c7289e660899df845e23d15089df8fa7a7f
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-12-03 13:15:14 -0500

    rango_ok

--- ejercicios-javascript-main/rtaEjer.js
+++ ejercicios-javascript-main/rtaEjer.js
@@ -145,3 +145,32 @@ console.log(fizzBuzz(6));
 console.log(fizzBuzz(20));
 console.log(fizzBuzz(30));
 console.log(fizzBuzz(8)); 
+
+
+/* ## 7. Contar rango de números
+
+Escribir una función llamada `contarRango` que reciba dos números y retorne cuántos números que hay entre ellos (excluyéndolos):
+
+**Nota:** Utiliza un ciclo en tu solución. Puedes asumir que el primer número va a ser menor que el segundo.
+
+```javascript
+// escribe tu respuesta acá
+
+// código de prueba
+console.log(contarRango(1, 9)) // 7
+console.log(contarRango(1332, 8743)) // 7410
+console.log(contarRango(5, 6)) // 0
+``` */
+function contarRango(n1, n2){
+    if(n1 < n2 ){
+        return (n2 - n1)-1;
+    }
+    else {
+        return (n1 - n2)-1;
+    }
+    
+}
+console.log(contarRango(1, 9)) // 7
+console.log(contarRango(1332, 8743)) // 7410
+console.log(contarRango(5, 6)) 
+console.log(contarRango(6, 4)) 
