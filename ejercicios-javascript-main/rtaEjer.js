49d3f725bacf7ce2dff605c9c45ea00e48384b46
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-12-06 15:11:32 -0500

    listo sumado entre rangos

--- ejercicios-javascript-main/rtaEjer.js
+++ ejercicios-javascript-main/rtaEjer.js
@@ -268,3 +268,57 @@ function multiplicarArreglo(array) {
 console.log(multiplicarArreglo([4, 1, 2, 3])); // 24
 console.log(multiplicarArreglo([1, 2, 3, 4, 5, 6, 7, 8])); // 40320
 console.log(multiplicarArreglo([])); // 1
+
+
+/* ## 13. Remover ceros
+
+Escribir una función llamada `removerCeros` que reciba un arreglo de números y retorne un nuevo arreglo excluyendo los ceros (0).
+
+```javascript
+// escribe tu respuesta acá */
+function removerCeros(array) {
+    let sinCero = [];
+    let n = 0;
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] != 0) {
+            n = array[i];
+            sinCero.push(n)
+        }
+    }
+    return sinCero;
+}
+// código de prueba
+console.log(removerCeros([0, 1, 0, 2, 0, 3])) // [1, 2, 3]
+console.log(removerCeros([9, 3, 6, 4])) // [9, 3, 6, 4]
+console.log(removerCeros([0, 0, 0])) // []
+
+/* ## 14. Sumar arreglo entre el rango
+
+Escribir una función llamada `sumarArreglo` que reciba tres argumentos: un arreglo de números, la posición inicial y la posición final. La función debe retornar la suma de todos los números dentro del rango (la posición inicial y la posición final, incluyéndolas).
+
+**Nota:** puedes asumir que la posición inicial va a ser menor o igual a la posición final, y que están dentro de los límites del arreglo.
+ 
+```javascript
+// escribe tu respuesta acá
+*/
+function sumarArregloEntre(arreglo,a,b) {
+    let acum = arreglo[a];
+    console.log(arreglo[a])
+    console.log(arreglo[b])
+    console.log(a)
+    if(a < b){
+        for (let i = arreglo[a]; i < arreglo[b]; i++) {
+            console.log(arreglo[i])    
+            acum += arreglo[i];
+            console.log(acum);
+        }
+    }else{
+        acum = 0;
+    }
+    
+    return acum
+}
+// código de prueba
+console.log(sumarArregloEntre([1, 2, 3], 1, 2)) // 5
+ console.log(sumarArregloEntre([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3, 6)) // 22
+console.log(sumarArregloEntre([7, 8, 9], 0, 0))  // 0
