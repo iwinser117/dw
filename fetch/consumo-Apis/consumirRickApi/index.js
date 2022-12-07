e1e61578a6ae26cd01332b05985d1b8e358968a5
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-12-07 03:14:01 -0500

    muy pilas con el path porque requiere comillas normales

--- fetch/consumo-Apis/consumirRickApi/index.js
+++ fetch/consumo-Apis/consumirRickApi/index.js
@@ -1,14 +1,26 @@
-const axios = require('axios')
-const API = 'https://rickandmortyapi.com/api/character'
-const main = async() => {
-    let response = await axios.get(API );
-    let characters = response.data.results.map((character) => {
-        return {
-            id: character.id,
-            name: character.name,
-        }
-    })
-    console.log(characters)
-}
+const axios = require("axios");
+const fs = require("fs").promises;
+const path = require("path");
+const { exec } = require("child_process");
+
+
+
+const API = "https://rickandmortyapi.com/api/character";
+const main = async () => {
+  let response = await axios.get(API);
+  let characters = response.data.results
+    .map((character) => {
+      return {
+        id: character.id,
+        name: character.name,
+        status: character.status,
+        species: character.species,
+      };
+    }).map((personaje)=> Object.values(personaje).join(",")).join("\n"); 
     
+    await fs.writeFile(path.join(__dirname,"data.csv"), characters);
+    console.log(__dirname,'data.csv');
+  console.log(characters);
+};
+
 main();
