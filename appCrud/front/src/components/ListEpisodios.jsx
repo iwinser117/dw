8e13bfcb1857c73b2d086c56e55fecc63121c238
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-12-19 19:09:34 -0500

    agregando el codigo Actual Dic 2021

--- appCrud/front/src/components/ListEpisodios.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-
-const ListEpisodios = () => {
-  return <li className="dropdown-item ">  {/* {ep.name}   */}lista de episodiso</li>;
-};
-
-export default ListEpisodios;
--- appCrud/front/src/containers/Layout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom'; 
-
-const Layout = ({ children }) => {
-  return <div className="Layout">{children}</div>;
-};
-export default Layout;
